5d68ca8433f0c2545126ef1e566579d6f603d83a
PrajyotGajane <prajyotgajane@gmail.com>
2020-06-17 17:25:26 +0530

    Added: class Person to store contact details

--- addressBook.java
+++ addressBook.java
@@ -1,7 +1,46 @@
 package com.bridgelabz.addressBook;
 
+import java.util.Scanner;
+
+//main class to choose various options
 public class addressBook {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Welcome to Address book");
+        System.out.println("Enter the first name:");
+        String firstName = sc.nextLine();
+        System.out.println("Enter the last name:");
+        String lastName = sc.nextLine();
+        System.out.println("Enter the City name:");
+        String cityName = sc.nextLine();
+        System.out.println("Enter the State name:");
+        String stateName = sc.nextLine();
+        System.out.println("Enter the zip code:");
+        int zipCode = sc.nextInt();
+        System.out.println("Enter the mobile number:");
+        long mobileNumber = sc.nextLong();
+        new Person( firstName, lastName, cityName, stateName, zipCode, mobileNumber);
+    }
+}
+//class to store contacts
+class Person{
+    public String firstName;
+    public String lastName;
+    public String cityName;
+    public String stateName;
+    public int zipCode;
+    public long mobileNumber;
+    Person( String firstName, String lastName,String cityName,String stateName,int zipCode,long
+            mobileNumber){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.cityName = cityName;
+        this.stateName = stateName;
+        this.zipCode = zipCode;
+        this.mobileNumber = mobileNumber;
+    }
+    public String toString(){
+        return this.firstName+" "+ this.lastName +" "+ this.cityName +" "+ this.stateName +" "+
+                this.zipCode +" "+ this.mobileNumber;
     }
 }
