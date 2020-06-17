705ae43cbaffac17aab70b9c4b24904461ccf0db
PrajyotGajane <prajyotgajane@gmail.com>
2020-06-17 18:16:46 +0530

    Added: Code for editing contact information

--- addressBook.java
+++ addressBook.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.addressBook;
 
 import java.util.ArrayList;
+import java.util.ListIterator;
 import java.util.Scanner;
 
 //main class to choose various options
@@ -13,6 +14,8 @@ public class addressBook {
         String firstName = sc.nextLine();
         System.out.println("Enter the last name:");
         String lastName = sc.nextLine();
+        System.out.println("Enter your address:");
+        String address = sc.nextLine();
         System.out.println("Enter the City name:");
         String cityName = sc.nextLine();
         System.out.println("Enter the State name:");
@@ -22,18 +25,58 @@ public class addressBook {
         System.out.println("Enter the mobile number:");
         long mobileNumber = sc.nextLong();
         //this reference can be used to store multiple contacts records in array list
-        arrayReference.add(new Person( firstName, lastName, cityName, stateName, zipCode, mobileNumber));
+        arrayReference.add(new Person( firstName, lastName, address, cityName, stateName, zipCode, mobileNumber));
+        System.out.println("Edit information ");
+        System.out.println("Enter the first name of the person you would like to edit details about");
+        sc.nextLine();
+        String update = sc.nextLine();
+        for(ListIterator<Person> iter = arrayReference.listIterator(); iter.hasNext();){
+            Person data = iter.next();
+            if(update.equals(data.getRecord())){
+                System.out.println("Update 1: Address 2:City 3:State 4:Zip code 5: Mobile Number");
+                int choice_2 = sc.nextInt();
+                switch (choice_2){
+                    case 1:
+                        sc.nextLine();
+                        System.out.println("Update new address:");
+                        data.cityName = sc.nextLine();
+                        break;
+                    case 2:
+                        System.out.println("Update the city:");
+                        data.cityName = sc.nextLine();
+                        break;
+                    case 3:
+                        System.out.println("Update the state");
+                        data.stateName = sc.nextLine();
+                        break;
+                    case 4:
+                        System.out.println("Update the mobile zip code");
+                        data.zipCode = sc.nextInt();
+                        break;
+                    case 5:
+                        System.out.println("Update the mobile number");
+                        data.mobileNumber = sc.nextLong();
+                    default:
+                        System.out.println("Enter valid input");
+                        break;
+                }
+                System.out.println("Contact updated");
+            }
+        }
+        for(Person cont : arrayReference)
+                System.out.println(cont);
     }
 }
 //class to store contacts
 class Person{
     public String firstName;
     public String lastName;
+    public String address;
     public String cityName;
     public String stateName;
     public int zipCode;
     public long mobileNumber;
-    Person( String firstName, String lastName,String cityName,String stateName,int zipCode,long
+    Person( String firstName, String lastName,String address, String cityName,String stateName,int zipCode,long
             mobileNumber){
         this.firstName = firstName;
         this.lastName = lastName;
@@ -45,6 +88,9 @@ class Person{
     public String toString(){
         return this.firstName+" "+ this.lastName +" "+ this.cityName +" "+ this.stateName +" "+
                 this.zipCode +" "+ this.mobileNumber;
+    }
+    public String getRecord(){
+        return this.firstName;
     }
 }
 
