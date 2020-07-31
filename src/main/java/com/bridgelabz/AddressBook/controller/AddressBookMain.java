9ef72244857cdc5863621eb75cc4b27b1f38f967
PrajyotGajane <prajyotgajane@gmail.com>
2020-07-31 09:10:29 +0530

    Refactored: Used Json.simple library to create json file for address book

--- src/main/java/com/bridgelabz/AddressBook/controller/AddressBookMain.java
+++ src/main/java/com/bridgelabz/AddressBook/controller/AddressBookMain.java
@@ -1,54 +1,53 @@
 package com.bridgelabz.AddressBook.controller;
+
 import com.bridgelabz.AddressBook.models.Person;
 import com.bridgelabz.AddressBook.service.AddressBook;
-import com.bridgelabz.AddressBook.service.JavaFileHandler;
 
 import java.util.*;
-public class AddressBookMain {
-    private static String JSON_FOR_ADDRESS_BOOK = "./JsonUsingJavaFileHandler.json";
 
-    public static void main(String[] args) {
-        List<Person> contactsDetailsList = new ArrayList<>();
-        HashMap<String,String> mapCity= new HashMap<>();
-        HashMap<String,String> mapState=new HashMap<>();
-        Scanner sc = new Scanner(System.in);
-        contactsDetailsList.add(new Person("Prajyot", "Gaj", "Hno 4", "Gogol", "Gta", 23232, 345345345));
-        boolean endKey = true;
-        System.out.println("Welcome to Address book");
-        System.out.println("Select options from the menu");
-        while (endKey) {
-            System.out.println("1.Add  2.Edit  3.Delete  4.View  5:Sort  6:Search      9:EXIT");
-            int choice = sc.nextInt();
-            switch (choice) {
-                case 1:
-                    AddressBook.addContact(contactsDetailsList, mapCity, mapState);
-                    break;
-                case 2:
-                    AddressBook.editContactDetails(contactsDetailsList);
-                    break;
-                case 3:
-                    AddressBook.deleteContact(contactsDetailsList);
-                    break;
-                case 4:
-                    AddressBook.viewContacts(contactsDetailsList, mapCity, mapState);
-                    break;
-                case 5:
-                    AddressBook.sortContact(contactsDetailsList);
-                    contactsDetailsList.stream().forEach(System.out::println);
-                    break;
-                case 6:
-                    AddressBook.customPlace(mapCity, mapState);
-                    break;
-                case 7:
-                    new JavaFileHandler(contactsDetailsList);
-                    break;
-                case 9:
-                    endKey=false;
-                    break;
-                default:
-                    System.out.println("Choose valid input");
-                    break;
+public class AddressBookMain {
+      public static void main(String[] args) {
+            List<Person> contactsDetailsList = new ArrayList<>();
+            HashMap<String, String> mapCity = new HashMap<>();
+            HashMap<String, String> mapState = new HashMap<>();
+            Scanner sc = new Scanner(System.in);
+            boolean endKey = true;
+            System.out.println("Welcome to Address book");
+            System.out.println("Select options from the menu");
+            contactsDetailsList.add(new Person("Prajyot", "Gaj", "Hno 4", "Gogol", "Gta", 23232, 345345345));
+            while (endKey) {
+                  System.out.println("1.Add  2.Edit  3.Delete  4.View  5:Sort  6:Search  7:Create Output Files    9:EXIT");
+                  int choice = sc.nextInt();
+                  switch (choice) {
+                        case 1:
+                              AddressBook.addContact(contactsDetailsList, mapCity, mapState);
+                              break;
+                        case 2:
+                              AddressBook.editContactDetails(contactsDetailsList);
+                              break;
+                        case 3:
+                              AddressBook.deleteContact(contactsDetailsList);
+                              break;
+                        case 4:
+                              AddressBook.viewContacts(contactsDetailsList, mapCity, mapState);
+                              break;
+                        case 5:
+                              AddressBook.sortContact(contactsDetailsList);
+                              contactsDetailsList.stream().forEach(System.out::println);
+                              break;
+                        case 6:
+                              AddressBook.customPlace(mapCity, mapState);
+                              break;
+                        case 7:
+                              AddressBook.writeToJson();
+                              break;
+                        case 9:
+                              endKey = false;
+                              break;
+                        default:
+                              System.out.println("Choose valid input");
+                              break;
+                  }
             }
-        }
-    }
+      }
 }
--- src/main/java/com/bridgelabz/AddressBook/service/AddressBook.java
+++ src/main/java/com/bridgelabz/AddressBook/service/AddressBook.java
@@ -1,14 +1,25 @@
 package com.bridgelabz.AddressBook.service;
 
 import com.bridgelabz.AddressBook.models.Person;
+import com.google.gson.JsonObject;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+
 public class AddressBook {
+      static JSONObject contactDetails = new JSONObject();
       /**
        * to add details to Person class
+       *
        * @return object of Person class
        */
       public static void addContact(List<Person> arrayReference, HashMap<String, String> mapCity, HashMap<String, String> mapState) {
@@ -18,11 +29,13 @@ public class AddressBook {
             try {
                   System.out.println("Enter the first name:");
                   String firstName = sc.nextLine();
+                  contactDetails.put("firstName", firstName);
                   validString = AddressBook.stringChecker(firstName);
                   if (validString) {
                         validString = false;
                         System.out.println("Enter the last name:");
                         String lastName = sc.nextLine();
+                        contactDetails.put("lastName", lastName);
                         validString = AddressBook.stringChecker(lastName);
                         if (validString) {
                               String firstLastName = firstName + lastName;
@@ -35,49 +48,58 @@ public class AddressBook {
                               if (flag) {
                                     System.out.println("Enter your address:");
                                     String address = sc.nextLine();
+                                    contactDetails.put("address", address);
                                     System.out.println("Enter the City name:");
                                     String cityName = sc.nextLine();
+                                    contactDetails.put("cityName", cityName);
                                     System.out.println("Enter the State name:");
                                     String stateName = sc.nextLine();
+                                    contactDetails.put("stateName", stateName);
                                     System.out.println("Enter the zip code:");
                                     int zipCode = sc.nextInt();
+                                    contactDetails.put("zipCode", zipCode);
                                     sc.nextLine();
                                     System.out.println("Enter the mobile number:");
                                     long mobileNumber = sc.nextLong();
+                                    contactDetails.put("mobileNumber", mobileNumber);
                                     arrayReference.add(new Person(firstName, lastName, address, cityName, stateName, zipCode, mobileNumber));
-                                    mapCity.put(firstName,cityName);
-                                    mapState.put(firstName,stateName);
+                                    mapCity.put(firstName, cityName);
+                                    mapState.put(firstName, stateName);
                               }
-                        } else{
+                        } else {
                               System.out.println("Enter valid last name");
                         }
-                  } else{
+                  } else {
                         System.out.println("Enter valid first name");
                   }
             } catch (InputMismatchException e) {
                   System.out.println("Enter valid data");
             }
       }
+
       /**
        * to check if the string is entirely made up of characters and not integers
+       *
        * @param checkString
        * @return validString
        */
-      public static boolean stringChecker(String checkString){
+      public static boolean stringChecker(String checkString) {
             Pattern stringChecker = Pattern.compile("([a-zA-Z]+)");
             Matcher matchString = stringChecker.matcher(checkString);
             boolean validString = matchString.matches();
             return validString;
       }
+
       /**
        * to sort the contacts as user wishes
+       *
        * @param arrayReference
        */
-      public static void sortContact(List<Person> arrayReference){
+      public static void sortContact(List<Person> arrayReference) {
             Scanner sc = new Scanner(System.in);
             System.out.println("Press 1: Sort by Name 2: Sort by city 3: Sort by State 4: Sort by zip code");
             int choice_2 = sc.nextInt();
-            switch (choice_2){
+            switch (choice_2) {
                   case 1:
                         List<Person> sortedList = arrayReference.stream()
                                 .sorted(Comparator.comparing(Person::getRecord))
@@ -108,36 +130,39 @@ public class AddressBook {
             }
             //AddressBookFunction.displayAll(arrayReference);
       }
+
       /**
        * to display all contacts
+       *
        * @param arrayReference
        */
-      public static void displayAll(List<Person> arrayReference){
+      public static void displayAll(List<Person> arrayReference) {
             arrayReference.stream().forEach(System.out::println);
       }
-      public static void viewContacts(List<Person> arrayReference, HashMap<String,String> mapCity, HashMap<String,String> mapState){
+
+      public static void viewContacts(List<Person> arrayReference, HashMap<String, String> mapCity, HashMap<String, String> mapState) {
             Scanner sc = new Scanner(System.in);
             System.out.println("1: All contacts 2: By city 3:By state");
             int choice3 = sc.nextInt();
-            switch (choice3){
+            switch (choice3) {
                   case 1:
                         //to view all contents of the address book
                         AddressBook.displayAll(arrayReference);
                         break;
                   case 2:
                         //to view people and their cities
-                        for (Iterator iterator = mapCity.entrySet().iterator(); iterator.hasNext();) {
+                        for (Iterator iterator = mapCity.entrySet().iterator(); iterator.hasNext(); ) {
                               Map.Entry e = (Map.Entry) iterator.next();
                               // prints all the people in map with their corresponding cities
-                              System.out.println(e.getKey()+" from "+e.getValue());
+                              System.out.println(e.getKey() + " from " + e.getValue());
                         }
                         break;
                   case 3:
                         //to view people and their states
-                        for (Iterator iterator = mapState.entrySet().iterator(); iterator.hasNext();) {
-                              Map.Entry e = (Map.Entry) iterator.next();
+                        for (Map.Entry<String, String> stringStringEntry : mapState.entrySet()) {
+                              Map.Entry e = stringStringEntry;
                               // prints all the people in map with their corresponding states
-                              System.out.println(e.getKey()+" from "+e.getValue());
+                              System.out.println(e.getKey() + " from " + e.getValue());
                         }
                         break;
                   default:
@@ -145,12 +170,14 @@ public class AddressBook {
                         break;
             }
       }
+
       /**
        * to search for people of particular city or state
+       *
        * @param mapCity
        * @param mapState
        */
-      public static void customPlace(HashMap<String,String> mapCity,HashMap<String,String> mapState) {
+      public static void customPlace(HashMap<String, String> mapCity, HashMap<String, String> mapState) {
             Scanner sc = new Scanner(System.in);
             System.out.println("Search people");
             System.out.println("1:By city    2:By state");
@@ -187,12 +214,14 @@ public class AddressBook {
                               System.out.println("Invalid input");
                               break;
                   }
-            } catch (InputMismatchException e){
+            } catch (InputMismatchException e) {
                   System.out.println("Enter valid option");
             }
       }
+
       /**
        * to delete contacts
+       *
        * @param arrayReference
        */
       public static void deleteContact(List<Person> arrayReference) {
@@ -209,8 +238,10 @@ public class AddressBook {
             System.out.println("Contact deleted");
             AddressBook.displayAll(arrayReference);
       }
+
       /**
        * edit details of contacts
+       *
        * @param arrayReference
        */
       public static void editContactDetails(List<Person> arrayReference) {
@@ -257,5 +288,17 @@ public class AddressBook {
                   }
             }
             AddressBook.displayAll(arrayReference);
+      }
+      public static void writeToJson() {
+            JSONObject personObject = new JSONObject();
+            personObject.put("personDetails", contactDetails);
+            JSONArray personList = new JSONArray();
+            personList.add(personObject);
+            try (FileWriter file = new FileWriter("./ContactList.json")) {
+                  file.write(personList.toString());
+                  file.flush();
+            } catch (IOException e) {
+                  e.printStackTrace();
+            }
       }
 }
